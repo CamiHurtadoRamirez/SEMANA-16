33c56509ded66138ef603f52a65bf229da3c9a4a
CamiHurtadoRamirez <159548522+CamiHurtadoRamirez@users.noreply.github.com>
2024-05-24 13:16:32 -0500

    Add files via upload

---
+++ SEMANA 16 CAMILA HURTADO/JS/LENGUAJE.js
@@ -0,0 +1,37 @@
+function mostrar (){
+    let r1,r2,r3,r4;
+    let cant=0;
+    if(document.getElementById('len01').checked){
+        r1="javascript";
+        cant++
+    }else{
+        r1=" ";
+    }
+    if(document.getElementById('len02').checked){
+        r2="python";
+        cant++
+    }else{
+        r2=" ";
+    }
+    if(document.getElementById('len03').checked){
+        r3="php";
+        cant++
+    }else{
+        r3=" ";
+    }
+    if(document.getElementById('len04').checked){
+        r4="C#";
+        cant++
+    }else{
+        r4=" ";
+    }
+    document.getElementById('resp').value=r1+" "+r2+" "+r3+" "+r4;
+    alert("LA CANTIDAD DE LENGUAJES SELECCIONADOS ES"+cant);
+}
+function borrar(){
+    document.getElementById('len01').checked=false;//borrar
+    document.getElementById('len02').checked=false;
+    document.getElementById('len03').checked=false;
+    document.getElementById('len04').checked=false;
+    document.getElementById('resp').value=" ";
+}
